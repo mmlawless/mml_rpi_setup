28c6a661dea891b4cd00dc70134d645f6726ca9a
mmlawless <72357026+mmlawless@users.noreply.github.com>
2025-11-13 17:51:46 +0000

    Update script version to 2025-11-13

--- mml_rpi_setup.sh
+++ mml_rpi_setup.sh
@@ -10,7 +10,7 @@ set -euo pipefail
 #           + Solid msmtp Gmail App Password flow
 ############################################################
 
-SCRIPT_VERSION="2025-10-24-Universal-Secure"
+SCRIPT_VERSION="2025-11-13"
 SCRIPT_HASH="PLACEHOLDER_HASH"
 
 # File paths
