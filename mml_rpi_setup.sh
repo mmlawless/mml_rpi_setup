042f24bd13da07f85da3cccef2e15caebbe46158
mmlawless <72357026+mmlawless@users.noreply.github.com>
2025-11-16 13:04:03 +0000

    Update mml_rpi_setup.sh

--- mml_rpi_setup.sh
+++ mml_rpi_setup.sh
@@ -165,7 +165,7 @@ run_neofetch_if_installed() {
 # Script variables/state and header banner
 ############################################################
 
-SCRIPT_VERSION="2025-11-16a"
+SCRIPT_VERSION="2025-11-16b"
 STATE_FILE="$HOME/.rpi_setup_state"
 CHECKPOINT_FILE="$HOME/.rpi_setup_checkpoint"
 LOG_FILE="$HOME/.rpi_setup.log"
